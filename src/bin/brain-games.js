e252b8d4e8286c4e1484451c82c4ebfa31e0003d
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2019-11-20 22:29:57 +0300

    Finish step 3. Add a greeting the user.

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import { greeting } from '..';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
+
+// Greeting the user.
+greeting();
