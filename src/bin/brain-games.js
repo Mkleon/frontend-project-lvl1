0926a7bd82b19855a3537f2eedfa22a282e10d8c
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2019-11-22 20:58:56 +0300

    Refactor the executable file.

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,7 +2,5 @@
 
 import greeting from '../index';
 
-console.log('Welcome to the Brain Games!\n');
-
 // Greeting the user.
 greeting();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
+  console.log('Welcome to the Brain Games!\n');
+
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
